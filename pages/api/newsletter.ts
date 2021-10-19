7ad160c7d8d7955db530c21d04ff37fed6e88c80
Vadim Kirpun <quinze105@gmail.com>
2021-10-20 01:03:25 +0300

    add db connection to newsletter

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,13 +1,25 @@
+import { MongoClient } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { checkIfEmailEmpty } from '../../helpers/validation';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const databaseName = 'nextjs-events';
+const credentials = `${process.env.DB_USER}:${process.env.DB_PASSWORD}`;
+const mongodbURL = `mongodb+srv://${credentials}@cluster0.nadat.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const { email } = req.body;
 
-    if (!email || !email.includes('@')) {
+    if (checkIfEmailEmpty(email)) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(mongodbURL);
+    const db = client.db();
+
+    await db.collection('emails').insertOne({ email });
+    await client.close();
 
     res.status(201).json({ message: 'Signed up!' });
   }
